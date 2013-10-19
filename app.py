280d181f4da623144d5aa442dd49ae3c2a594ead
Gaurav Ahuja <gauravahuja9@gmail.com>
2013-10-19 19:50:35 -0400

    Flask boiler plate cleanup, added google map

--- app.py
+++ app.py
@@ -3,10 +3,8 @@
 #----------------------------------------------------------------------------#
 
 from flask import * # do not use '*'; actually input the dependencies.
-from flask.ext.sqlalchemy import SQLAlchemy
 import logging
 from logging import Formatter, FileHandler
-from forms import *
 
 #----------------------------------------------------------------------------#
 # App Config.
@@ -14,53 +12,14 @@ from forms import *
 
 app = Flask(__name__)
 app.config.from_object('config')
-#db = SQLAlchemy(app)
 
-# Automatically tear down SQLAlchemy.
-'''
-@app.teardown_request
-def shutdown_session(exception=None):
-    db_session.remove()
-'''
-
-# Login required decorator.
-'''
-def login_required(test):
-    @wraps(test)
-    def wrap(*args, **kwargs):
-        if 'logged_in' in session:
-            return test(*args, **kwargs)
-        else:
-            flash('You need to login first.')
-            return redirect(url_for('login'))
-    return wrap
-'''
 #----------------------------------------------------------------------------#
 # Controllers.
 #----------------------------------------------------------------------------#
 
 @app.route('/')
 def home():
-    return render_template('pages/placeholder.home.html')
-
-@app.route('/about')
-def about():
-    return render_template('pages/placeholder.about.html')
-
-@app.route('/login')
-def login():
-    form = LoginForm(request.form)
-    return render_template('forms/login.html', form = form)
-
-@app.route('/register')
-def register():
-    form = RegisterForm(request.form)
-    return render_template('forms/register.html', form = form)
-
-@app.route('/forgot')
-def forgot():
-    form = ForgotForm(request.form)
-    return render_template('forms/forgot.html', form = form)
+    return render_template('pages/index.html')
 
 # Error handlers.
 
--- config.py
+++ config.py
@@ -10,5 +10,3 @@ DEBUG = True
 # http://clsc.net/tools-old/random-string-generator.php
 SECRET_KEY = 'my precious'
 
-# Connect to the database
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
